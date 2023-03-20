acd415b62bfb288fe28274e94dccfbb2edd8a09e
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-03-20 13:34:33 +0530

    update mapbox-gl

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,4 +1,5 @@
-import mapboxgl from "mapbox-gl";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from "!mapbox-gl";
 import { useEffect, useRef, useState } from "react";
 import "./map.css";
 import Render3D from "../render-3d/Render3D";
